ee1e3dc5c1e4c1e8b9fa891ac6fb106ed9b6f985
yasniquele <yasmin.mugnaini@escola.pr.gov.br>
2025-05-30 22:06:35 +0000

    AULA 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'O que significa o nome da Fontana di Trevi?',
+  'O nome vem de Tre Vie, que significa três ruas, e a tradição é jogar uma moeda de costas para a fonte com a mão direita por cima do ombro esquerdo'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Como surgiu a massa na culinária italiana?',
+  'A massa surgiu quando civilizações antigas, como os etruscos, esmagavam cereais, misturavam com água e cozinhavam, criando um alimento nutritivo'
+)
+
+criaCartao(
+  'Curiosidades sobre a Itália',
+  'Por que o vinho é importante na Itália?',
+  'O vinho está presente em todas as refeições e celebrações, a Itália é o maior exportador de vinho do mundo e tem mais de 350 tipos de uvas nativas'
+)
